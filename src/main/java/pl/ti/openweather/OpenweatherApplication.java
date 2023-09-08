a971d95a716328cd50e888186c375422e73babed
Tomasz Iwanicki <tiko35@o2.pl>
2023-09-08 22:15:11 +0200

    Initial commit

---
+++ src/main/java/pl/ti/openweather/OpenweatherApplication.java
@@ -0,0 +1,13 @@
+package pl.ti.openweather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenweatherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenweatherApplication.class, args);
+    }
+
+}
